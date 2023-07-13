8ae609d8da860f657567e37b5bd60bb43ef87890
Pedro Andrade <pdandradeb@gmail.com>
2023-07-13 10:56:07 -0300

    configuring builds

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,7 @@ import { ConfigContext, ExpoConfig } from 'expo/config';
 import { version, versionCode } from './version.json';
 
 const env = process.env.EXPO_PUBLIC_ENV;
+const projectId = process.env.EXPO_PUBLIC_EAS_PROJECT_ID;
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
@@ -24,10 +25,12 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     icon: './assets/images/notification-icon.png',
   },
   ios: {
+    bundleIdentifier: appBundlePackage(),
     buildNumber: `${versionCode}`,
     supportsTablet: false,
   },
   android: {
+    package: appBundlePackage(),
     versionCode,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
@@ -39,10 +42,21 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     tsconfigPaths: true,
     typedRoutes: true,
   },
+  updates: {
+    url: `https://u.expo.dev/${projectId}`,
+  },
+  runtimeVersion: {
+    policy: 'sdkVersion',
+  },
+  extra: {
+    eas: {
+      projectId,
+    },
+  },
 });
 
 const name = () => {
-  const name = 'Entregas';
+  const name = 'AppJusto Entregador';
   if (!env || env === 'live') return name;
   return `(${env.charAt(0).toUpperCase()}) ${name}`;
 };
@@ -57,4 +71,8 @@ const scheme = () => {
   const scheme = 'appjustocourier';
   if (!env || env === 'live') return scheme;
   return `${scheme}${env}`;
+};
+
+const appBundlePackage = () => {
+  return `br.com.appjusto.courier.${env}`;
 };
