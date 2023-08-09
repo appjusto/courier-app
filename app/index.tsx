172caaa189943b241d1730c886c492b5396506f0
Pedro Andrade <pdandradeb@gmail.com>
2023-08-08 23:36:12 -0300

    Adding lucide icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { DefaultText } from '@/common/components/texts/DefaultText';
 import { Link } from 'expo-router';
+import { Camera } from 'lucide-react-native';
 import { Pressable, View } from 'react-native';
 
 export default function Welcome() {
@@ -12,6 +13,9 @@ export default function Welcome() {
           <DefaultText size="2xl">Entrar!</DefaultText>
         </Pressable>
       </Link>
+      <View>
+        <Camera color="red" size={48} />
+      </View>
     </View>
   );
 }
