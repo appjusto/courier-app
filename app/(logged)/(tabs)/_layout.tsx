6ce3ea1d503d3bf04ad96c796197bf6eaebe5aa8
Pedro Andrade <pdandradeb@gmail.com>
2023-08-28 18:47:51 -0300

    Tab bar background

--- app/(logged)/(tabs)/_layout.tsx
+++ app/(logged)/(tabs)/_layout.tsx
@@ -1,10 +1,30 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, View, useColorScheme } from 'react-native';
+import { Pressable, View, ViewProps, useColorScheme } from 'react-native';
 
+import borders from '@/common/styles/borders';
 import colors from '@/common/styles/colors';
 import Colors from '@/common/styles/themes';
 import { Home, Receipt, Users2 } from 'lucide-react-native';
+
+interface TabIconProps extends ViewProps {
+  focused?: boolean;
+}
+
+const TabIcon = ({ focused, children, ...props }: TabIconProps) => (
+  <View
+    style={{
+      width: 64,
+      ...borders.default,
+      borderColor: focused ? colors.neutral100 : colors.white,
+      backgroundColor: focused ? colors.neutral100 : colors.white,
+      alignItems: 'center',
+    }}
+    {...props}
+  >
+    {children}
+  </View>
+);
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -22,9 +42,9 @@ export default function TabLayout() {
         options={{
           title: 'Início',
           tabBarIcon: ({ focused }) => (
-            <View style={{ backgroundColor: focused ? colors.neutral100 : undefined }}>
+            <TabIcon focused={focused}>
               <Home size={24} color={focused ? activeColor : inactiveColor} />
-            </View>
+            </TabIcon>
           ),
           headerRight: () => (
             <Link href="/matching" asChild>
@@ -47,9 +67,9 @@ export default function TabLayout() {
         options={{
           title: 'Suas corridas',
           tabBarIcon: ({ focused }) => (
-            <View style={{ backgroundColor: focused ? colors.neutral100 : undefined }}>
+            <TabIcon focused={focused}>
               <Receipt size={24} color={focused ? activeColor : inactiveColor} />
-            </View>
+            </TabIcon>
           ),
         }}
       />
@@ -61,9 +81,9 @@ export default function TabLayout() {
           // tabBarActiveBackgroundColor: colors.neutral100,
           // tabBarBackground: () => <View style={{ backgroundColor: colors.neutral100 }} />,
           tabBarIcon: ({ focused }) => (
-            <View style={{ backgroundColor: focused ? colors.neutral100 : undefined }}>
+            <TabIcon focused={focused}>
               <Users2 size={24} color={focused ? activeColor : inactiveColor} />
-            </View>
+            </TabIcon>
           ),
         }}
       />
