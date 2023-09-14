a966f4d567a92900998aa943fc5cddfd8e1db19d
Pedro Andrade <pdandradeb@gmail.com>
2023-09-14 14:02:18 -0300

    Updating version

--- api/location/latlngFromLocation.ts
+++ api/location/latlngFromLocation.ts
@@ -1,6 +1,11 @@
 import { LatLng } from '@appjusto/types';
 import { Location } from 'react-native-background-geolocation';
 
-export const latlngFromLocation = (location: Location): LatLng => {
-  return { latitude: location.coords.latitude, longitude: location.coords.longitude };
+export const latlngFromLocation = (location: Location) => {
+  try {
+    return { latitude: location.coords.latitude, longitude: location.coords.longitude } as LatLng;
+  } catch (error: unknown) {
+    console.log(error);
+    return undefined;
+  }
 };
