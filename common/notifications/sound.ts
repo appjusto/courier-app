3ee9f86a010e33a618d7c39290ddc3b202f9ad97
Pedro Andrade <pdandradeb@gmail.com>
2023-09-22 19:26:16 -0300

    Fixing regression playing sound in background

--- common/notifications/sound.ts
+++ common/notifications/sound.ts
@@ -1,24 +1,22 @@
+import { Asset } from 'expo-asset';
 import { Audio } from 'expo-av';
 import { Sound } from 'expo-av/build/Audio';
-
 export const AUDIO_PATH = '../../assets/sounds/order_request.wav';
 
 let _sound: Sound;
 const getSound = async () => {
   if (!_sound) {
-    _sound = (await Audio.Sound.createAsync(require(AUDIO_PATH))).sound;
+    _sound = (await Audio.Sound.createAsync(Asset.fromModule(require(AUDIO_PATH)))).sound;
   }
   return _sound;
 };
 
 export const playOrderRequestSound = async () => {
-  console.log('playOrderRequestSound');
   const sound = await getSound();
   await sound.playAsync();
 };
 
 export const stopOrderRequestSound = async () => {
-  console.log('stopOrderRequestSound');
   if (!_sound) return;
   await _sound.stopAsync();
 };
