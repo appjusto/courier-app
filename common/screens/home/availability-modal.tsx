1c87dae5f0118ee8e7415f4875ac6680ebf6034e
Pedro Andrade <pdandradeb@gmail.com>
2023-10-03 07:44:04 -0300

    Adjusting AvailabilityModal UI

--- common/screens/home/availability-modal.tsx
+++ common/screens/home/availability-modal.tsx
@@ -33,61 +33,90 @@ export const AvailabilityModal = ({ onConfirm, onDismiss, ...props }: Props) => 
             }}
           >
             <View style={{ padding: paddings.lg, backgroundColor: colors.white }}>
-              <DefaultText size="xl">Como você vai fazer suas entregas hoje?</DefaultText>
-              <View style={{ flexDirection: 'row', marginVertical: paddings['2xl'] }}>
-                <OnlyIconButton
-                  style={{}}
-                  iconStyle={{
-                    borderWidth: 0,
-                    backgroundColor:
-                      selectedMode === 'motorcycle' ? colors.success100 : colors.neutral100,
-                  }}
-                  icon={
-                    <HelmetLargeIcon
-                      color={selectedMode === 'motorcycle' ? colors.success500 : colors.neutral900}
-                    />
-                  }
-                  size={70}
-                  variant="circle"
-                  onPress={() => setSelectedMode('motorcycle')}
-                />
-                <OnlyIconButton
-                  style={{
-                    marginLeft: paddings.lg,
-                  }}
-                  iconStyle={{
-                    borderWidth: 0,
-                    backgroundColor:
-                      selectedMode === 'bicycling' ? colors.success100 : colors.neutral100,
-                  }}
-                  icon={
-                    <Bike
-                      size={33}
-                      color={selectedMode === 'bicycling' ? colors.success500 : colors.neutral900}
-                    />
-                  }
-                  size={70}
-                  variant="circle"
-                  onPress={() => setSelectedMode('bicycling')}
-                />
-                <OnlyIconButton
-                  style={{
-                    marginLeft: paddings.lg,
-                  }}
-                  iconStyle={{
-                    borderWidth: 0,
-                    backgroundColor: selectedMode === 'car' ? colors.success100 : colors.neutral100,
-                  }}
-                  icon={
-                    <CarFront
-                      size={33}
-                      color={selectedMode === 'car' ? colors.success500 : colors.neutral900}
-                    />
-                  }
-                  size={70}
-                  variant="circle"
-                  onPress={() => setSelectedMode('car')}
-                />
+              <View
+                style={{
+                  alignSelf: 'center',
+                  marginTop: paddings.md,
+                  height: 6,
+                  width: 50,
+                  borderRadius: 100,
+                  backgroundColor: colors.neutral100,
+                }}
+              />
+              <DefaultText style={{ marginTop: paddings['2xl'] }} size="xl">
+                Como você vai fazer suas entregas hoje?
+              </DefaultText>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginVertical: paddings['2xl'],
+                }}
+              >
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                  <OnlyIconButton
+                    style={{}}
+                    iconStyle={{
+                      borderWidth: 0,
+                      backgroundColor:
+                        selectedMode === 'motorcycle' ? colors.success100 : colors.neutral100,
+                    }}
+                    icon={
+                      <HelmetLargeIcon
+                        color={
+                          selectedMode === 'motorcycle' ? colors.success500 : colors.neutral900
+                        }
+                      />
+                    }
+                    size={70}
+                    variant="circle"
+                    onPress={() => setSelectedMode('motorcycle')}
+                  />
+                  <DefaultText style={{ marginTop: paddings.xs }} size="md" color="black">
+                    Moto
+                  </DefaultText>
+                </View>
+                <View style={{ alignItems: 'center', marginLeft: paddings.xl }}>
+                  <OnlyIconButton
+                    iconStyle={{
+                      borderWidth: 0,
+                      backgroundColor:
+                        selectedMode === 'bicycling' ? colors.success100 : colors.neutral100,
+                    }}
+                    icon={
+                      <Bike
+                        size={33}
+                        color={selectedMode === 'bicycling' ? colors.success500 : colors.neutral900}
+                      />
+                    }
+                    size={70}
+                    variant="circle"
+                    onPress={() => setSelectedMode('bicycling')}
+                  />
+                  <DefaultText style={{ marginTop: paddings.xs }} size="md" color="black">
+                    Bike
+                  </DefaultText>
+                </View>
+                <View style={{ alignItems: 'center', marginLeft: paddings.xl }}>
+                  <OnlyIconButton
+                    iconStyle={{
+                      borderWidth: 0,
+                      backgroundColor:
+                        selectedMode === 'car' ? colors.success100 : colors.neutral100,
+                    }}
+                    icon={
+                      <CarFront
+                        size={33}
+                        color={selectedMode === 'car' ? colors.success500 : colors.neutral900}
+                      />
+                    }
+                    size={70}
+                    variant="circle"
+                    onPress={() => setSelectedMode('car')}
+                  />
+                  <DefaultText style={{ marginTop: paddings.xs }} size="md" color="black">
+                    Carro
+                  </DefaultText>
+                </View>
               </View>
               <DefaultButton
                 style={{ marginVertical: paddings.md }}
